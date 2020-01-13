3493faf18151cc4e0e1d30f2c278f849a432f026
Tim Morgan <tmorgan@onemedical.com>
2020-01-12 17:45:58 -0800

    Ruby 2.7, gem updates, linting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-const erb =  require('./loaders/erb')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const typescript =  require('./loaders/typescript')
+const erb = require('./loaders/erb')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('erb', erb)
 environment.loaders.prepend('typescript', typescript)
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
